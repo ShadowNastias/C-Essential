07e2c5283979cfa6ef2287514cd9e526858c8685
Anastasie Hubina <anastasiehubina@gmail.com>
2023-11-08 22:59:27 +0200

    Add code for library program.

---
+++ Library/Content.cs
@@ -0,0 +1,15 @@
+﻿namespace Library;
+
+public class Content
+{
+    public string content;
+
+    public void Show()
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine(content);
+        
+        // Resetting color of the text to make it default for other texts.
+        Console.ResetColor();
+    }
+}
---
+++ Library/Title.cs
@@ -0,0 +1,15 @@
+﻿namespace Library;
+
+public class Title
+{
+    public string title;
+
+    public void Show()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(title);
+        
+        // Resetting color of the text to make it default for other texts.
+        Console.ResetColor();
+    }
+}
